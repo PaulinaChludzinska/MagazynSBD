faa7cc510fe3a2a730b28d473d83b772c3835082
p_ch <44525373+PaulinaChludzinska@users.noreply.github.com>
2018-12-22 09:31:35 +0100

    Add files via upload

---
+++ Magazyn.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Magazyn;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Magazyn {
+    
+    private int id_magazyn;
+    
+    public Magazyn(){}
+
+    public int getId_magazyn() {
+        return id_magazyn;
+    }
+
+    public void setId_magazyn(int id_magazyn) {
+        this.id_magazyn = id_magazyn;
+    }
+    
+    
+}
